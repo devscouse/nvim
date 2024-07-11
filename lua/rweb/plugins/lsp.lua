3a3e65a63d8623ef069d2cec24f8006600ee5f7b
Rob-J <Rob-JWebster@outlook.com>
2024-07-11 20:33:53 +0100

    Turn off autocomplete in comments

--- lua/rweb/plugins/lsp.lua
+++ lua/rweb/plugins/lsp.lua
@@ -68,6 +68,16 @@ return {
 
         -- Set up completion plugin
         cmp.setup({
+            enabled = function()
+                -- disable completion in comments
+                local context = require 'cmp.config.context'
+                if vim.api.nvim_get_mode().mode == 'c' then
+                    return true
+                else
+                    return not context.in_treesitter_capture("comment")
+                        and not context.in_syntax_group("Comment")
+                end
+            end,
             snippet = {
                 expand = function(args)
                     luasnip.lsp_expand(args.body)
